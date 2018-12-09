a5aa12c9c686c0008d676d57709b20e5e2c98f4c
David Mohamad <davidkmohamad@gmail.com>
2018-12-09 19:58:02 +0000

    Bugfix: update dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name="sleepcounter",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
-        'linearstage==0.1.1',
+        'linearstage==0.2.0',
         'max7219'
         ],
 )
