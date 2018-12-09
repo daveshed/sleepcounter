a76942ccb230abecf2b437cd659c310dfd598ebb
David Mohamad <davidkmohamad@gmail.com>
2018-12-09 16:04:41 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name="sleepcounter",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
-        'linearstage==0.1.0',
+        'linearstage==0.1.1',
         'max7219'
         ],
 )
