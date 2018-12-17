be34a7bdeaf966ecd63f0eb9afcad0ca5b9f0718
David Mohamad <davidkmohamad@gmail.com>
2018-12-17 21:02:07 +0000

    Clear display on update

--- sleepcounter/widget/display.py
+++ sleepcounter/widget/display.py
@@ -9,8 +9,9 @@ LOGGER = logging.getLogger("display widget")
 
 
 display = matrix(cascaded=4)
-# we need to create display then set speed and then recreate so that spped is correct when needed.
-# better would be to create the spi instance and pass this into the constructor to instantiate matrix
+# we need to create display then set speed and then recreate so that spped is
+# correct when needed.# better would be to create the spi instance and pass this
+# into the constructor to instantiate matrix
 display._spi.max_speed_hz = 7800000
 display = matrix(cascaded=4)
 display.orientation(90)
@@ -33,6 +34,7 @@ class LedMatrixWidget(BaseWidget):
         LOGGER.info(
             "Updating with calendar {}. Setting message to {}"
             .format(calendar, msg))
+        display.clear()       
         display.show_message(msg)
         LOGGER.info("Setting message to {}".format(sleeps))
         display.show_message(str(sleeps))
