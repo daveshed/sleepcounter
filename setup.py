862676f68254018707febc302777ae3f630c83c5
David Mohamad <davidkmohamad@gmail.com>
2019-06-09 11:20:16 +0100

    bump version 2.2.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="sleepcounter",
-    version="2.2.3",
+    version="2.2.4",
     packages=find_packages(),
     install_requires=[
         'linearstage>=0.2.1',
